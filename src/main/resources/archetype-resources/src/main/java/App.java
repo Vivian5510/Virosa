2d9c2d6f863ceb0da00f367b5df0de211b661265
Xiang Xin Kai <2156722358@qq.com>
2024-12-04 22:07:26 +0800

    创建部分子模块 接入常用工具类

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.rosy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ virosa-utilis/src/main/java/com/rosy/virosa/utilis/Test.java
@@ -0,0 +1,39 @@
+package com.rosy.virosa.utilis;
+
+
+import cn.hutool.core.collection.CollUtil;
+import com.alibaba.fastjson2.JSON;
+import com.google.common.base.Throwables;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        try {
+            UtilisTest utilisTest1 = UtilisTest.builder().name("rosy").age(22).build();
+            UtilisTest utilisTest2 = UtilisTest.builder().name("vivian").age(21).build();
+            List<UtilisTest> utilisTestList = Arrays.asList(utilisTest1, utilisTest2);
+            if (CollUtil.isNotEmpty(utilisTestList)) {
+                System.out.println(JSON.toJSONString(utilisTestList));
+            }
+        } catch (Exception e) {
+            System.out.println(Throwables.getStackTraceAsString(e));
+        }
+
+    }
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class UtilisTest {
+    String name;
+    int age;
+}
