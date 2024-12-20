8928ff9dcdd79d6d9670cf1043e5a6c1f3e8a461
Xiang Xin Kai <2156722358@qq.com>
2024-12-20 23:27:23 +0800

    完成：1.Menu相关接口

--- virosa-admin/src/main/java/com/rosy/virosa/admin/controller/MenuController.java
+++ virosa-admin/src/main/java/com/rosy/virosa/admin/controller/MenuController.java
@@ -1,15 +1,40 @@
 package com.rosy.virosa.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.rosy.virosa.common.domain.ResponseResult;
+import com.rosy.virosa.common.domain.entity.Menu;
+import com.rosy.virosa.common.domain.vo.MenuVo;
 import com.rosy.virosa.common.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/menu")
+@RequestMapping("/content/menu")
 public class MenuController {
 
     @Autowired
     private MenuService menuService;
 
+    @PostMapping
+    public ResponseResult addMenu(@RequestBody Menu menu) {
+        menuService.save(menu);
+        return ResponseResult.okResult();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseResult updateMenu(@RequestBody Menu menu) {
+        menuService.updateById(menu);
+        return ResponseResult.okResult();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseResult deleteMenu(@RequestBody Menu menu) {
+        menuService.removeById(menu.getId());
+        return ResponseResult.okResult();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseResult getMenu(@PathVariable Long id) {
+        return ResponseResult.okResult(BeanUtil.copyProperties(menuService.getById(id), MenuVo.class));
+    }
 }
--- virosa-common/src/main/java/com/rosy/virosa/common/domain/entity/SubMenuVo.java
+++
@@ -1,4 +0,0 @@
-package com.rosy.virosa.common.domain.entity;
-
-public class SubMenuVo {
-}
